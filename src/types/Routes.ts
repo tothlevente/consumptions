e737c80632f9ab70d1bbf438c56cdfae404e1f11
tothlevente <developer.tothlevente@gmail.com>
2025-03-30 18:46:51 +0200

    Add ROUTES type definition for application navigation

---
+++ src/types/Routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  HOME: "/",
+  CREATE_ACCOUNT: "/create-account",
+  FORGOT_PASSWORD: "/forgot-password",
+  PASSWORD_RECOVERY: "/password-recovery",
+  DASHBOARD: "/dashboard",
+};
