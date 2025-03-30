09c34b31a4f3bbc1fd917f667eefc5f62b9d9099
tothlevente <developer.tothlevente@gmail.com>
2025-03-30 18:34:29 +0200

    Implement routing and theming in the main application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
+import { Toaster } from "@/components/ui/sonner";
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <ThemeProvider defaultTheme="system">
+        <div className="site-wrapper">
+          <Header />
+          <div className="wrapper"></div>
+          <Footer />
+          <Toaster richColors />
+        </div>
+      </ThemeProvider>
+    </Router>
+  );
 };
